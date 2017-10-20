ce182c4dbfe6df012e642a0467367bec78ec289a
baoweinan <452287388@qq.com>
2017-10-20 22:55:59 +0800

    第一天

---
+++ index.js
@@ -0,0 +1,39 @@
+// import redux, { createStore } from 'redux'
+const redux = require('redux')
+const createStore = redux.createStore
+// 首先我们需要创建一个reducer来处理state
+// 注意 reducer一定是一个纯函数
+/**
+ * 
+ * @param {接受的state} state 
+ * @param {传入的action} action 
+ */
+const initState = {
+  myState: [
+    "我是初始的state"
+  ]
+}
+function myReducer(state = initState, action) {
+  switch (action.type) {
+    case "changeState":
+      // redux中一定要返回一个新的出去state，不可以改变原来的state
+      return Object.assign({}, state, { myState: ["我是改变的state"] })
+      break;
+    default:
+      return state
+      break;
+  }
+}
+// 将reducer放进createStore里 生成一个唯一的store
+const store = createStore(myReducer)
+// 通过store的getState方法我们可以看到store里的state
+console.log('初始的state', store.getState())
+
+//第二步我们通过创建一个action来让redux dispatch 这个action 去调用reducer
+// action单纯的返回一个必须包含type属性的对象   like:{type:'changeState'}
+function myAction() {
+  return{ type: 'changeState' }
+}
+// 第三步 我们调用dispath 来更改state
+store.dispatch(myAction())
+console.log('我是改变之后的state', store.getState())
